60dbd25d3546d1505638f3d4cafe6df123ac493d
Mark Garnick <markgman008@gmail.com>
2018-12-10 20:28:39 -0700

    add mock api and populate coursesPage.js

---
+++ src/components/course/CourseListRow.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from "react";
+import { Link } from "react-router";
+import CoursesPage from "./CoursesPage";
+
+const CourseListRow = ({ course }) => {
+  return (
+    <tr>
+      <td>
+        <a href={course.watchHref} target="_blank">
+          Watch
+        </a>
+      </td>
+      <td>
+        <Link to={"/course/" + course.id}>{course.title}</Link>
+      </td>
+      <td>{course.authorId}</td>
+      <td>{course.category}</td>
+      <td>{course.length}</td>
+    </tr>
+  );
+};
+
+CourseListRow.proptypes = {
+  course: PropTypes.object.isRequired
+};
+
+export default CourseListRow;
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,27 +1,12 @@
 import React, { PropTypes, Component } from "react";
 import { connect } from "react-redux";
-import { bindActionCreators } from 'redux';
+import { bindActionCreators } from "redux";
 import * as courseActions from "../../actions/courseActions";
+import CourseList from "./CourseList";
 
 class CoursesPage extends Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: "" }
-    };
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -29,24 +14,18 @@ class CoursesPage extends Component {
   }
 
   render() {
+    const { courses } = this.props;
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Courses</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="save" onClick={this.onClickSave} />
+        <CourseList courses={courses} />
       </div>
     );
   }
 }
 CoursesPage.propTypes = {
-    courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -56,9 +35,12 @@ function mapStateToProps(state, ownProps) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(courseActions, dispatch)
-    };
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
